e30f1a9caa6e0656aef81246f14d7080c7f4b04b
Ivan <ivantolmachevmaster@gmail.com>
2023-10-23 12:25:18 +0300

    HOPE_2

--- src/lib/components/CodeBlock.svelte
+++ src/lib/components/CodeBlock.svelte
@@ -1,21 +1,22 @@
 <script>
 	import Prism from "prismjs";
-	// import "prismjs/components/prism-rust.js";
-	// import "prismjs/components/prism-pug.js";
-	// import "prismjs/components/prism-clike.js";
-	// import "prismjs/components/prism-c.js";
-	// import "prismjs/components/prism-cpp.js";
-	// import "prismjs/components/prism-bash.js";
-	// import "prismjs/components/prism-python.js";
+	import "prismjs/components/prism-rust.js";
+	import "prismjs/components/prism-pug.js";
+	import "prismjs/components/prism-clike.js";
+	import "prismjs/components/prism-c.js";
+	import "prismjs/components/prism-cpp.js";
+	import "prismjs/components/prism-bash.js";
+	import "prismjs/components/prism-python.js";
 	import "prismjs/themes/prism-dark.min.css";
 
 	export let language;
 	export let code;
-	const html = Prism.highlight(
-		code.trim(),
-		Prism.languages[language],
-		language
-	);
+	let html = "";
+	if (Prism.languages[language]) {
+		html = Prism.highlight(code.trim(), Prism.languages[language], language);
+	} else {
+		html = `<span>${code}</span>`;
+	}
 </script>
 
 <pre class={"language-" + language}><code class={"language-" + language}
