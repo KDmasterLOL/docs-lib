eb8fdb4b9f7385a0785b45a3ad7cd22eb4316a52
Ivan <ivantolmachevmaster@gmail.com>
2023-10-23 14:03:18 +0300

    HOPE_5

--- dist/components/CodeBlock.svelte
+++ dist/components/CodeBlock.svelte
@@ -1,12 +1,12 @@
 <script>
 	import Prism from "prismjs";
-	import "prismjs/components/prism-rust.js";
-	import "prismjs/components/prism-pug.js";
-	import "prismjs/components/prism-clike.js";
-	import "prismjs/components/prism-c.js";
-	import "prismjs/components/prism-cpp.js";
-	import "prismjs/components/prism-bash.js";
-	import "prismjs/components/prism-python.js";
+	// import "prismjs/components/prism-rust.js";
+	// import "prismjs/components/prism-pug.js";
+	// import "prismjs/components/prism-clike.js";
+	// import "prismjs/components/prism-c.js";
+	// import "prismjs/components/prism-cpp.js";
+	// import "prismjs/components/prism-bash.js";
+	// import "prismjs/components/prism-python.js";
 	import "prismjs/themes/prism-dark.min.css";
 
 	/**
@@ -15,11 +15,12 @@
 	export let language;
 	export let code;
 	let html = "";
-	// if (Prism.languages && Prism.languages[language]) {
-	html = Prism.highlight(code.trim(), Prism.languages[language], language);
-	// } else {
-	// 	html = `<span>${code}</span>`;
-	// }
+	if (Prism.languages && Prism.languages[language]) {
+		html = Prism.highlight(code.trim(), Prism.languages[language], language);
+	} else {
+		html = `<span>${code}</span>`;
+		console.log("Not find " + language);
+	}
 </script>
 
 <pre class={"language-" + language}><code class={"language-" + language}
