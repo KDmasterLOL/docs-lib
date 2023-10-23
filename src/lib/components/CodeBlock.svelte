a4420782ae62a80de851ad9adf0910f87e31ebac
Ivan <ivantolmachevmaster@gmail.com>
2023-10-23 10:46:44 +0300

    HOPE_@

--- src/lib/components/CodeBlock.svelte
+++ src/lib/components/CodeBlock.svelte
@@ -1,11 +1,12 @@
 <script>
 	import Prism from "prismjs";
-	import "prismjs/components/prism-rust.js";
-	import "prismjs/components/prism-pug.js";
-	import "prismjs/components/prism-c.js";
-	import "prismjs/components/prism-cpp.js";
-	import "prismjs/components/prism-bash.js";
-	import "prismjs/components/prism-python.js";
+	// import "prismjs/components/prism-rust.js";
+	// import "prismjs/components/prism-pug.js";
+	// import "prismjs/components/prism-clike.js";
+	// import "prismjs/components/prism-c.js";
+	// import "prismjs/components/prism-cpp.js";
+	// import "prismjs/components/prism-bash.js";
+	// import "prismjs/components/prism-python.js";
 	import "prismjs/themes/prism-dark.min.css";
 
 	export let language;
