6d556edd9d7fea02c8acd72c92c32bdf16c803bc
Ivan <ivantolmachevmaster@gmail.com>
2023-10-26 19:40:53 +0300

    Correct the code block

--- src/lib/components/CodeBlock.svelte
+++ src/lib/components/CodeBlock.svelte
@@ -1,12 +1,12 @@
 <script>
 	import Prism from "prismjs";
-	// import "prismjs/components/prism-rust.js";
-	// import "prismjs/components/prism-pug.js";
-	// import "prismjs/components/prism-clike.js";
-	// import "prismjs/components/prism-c.js";
-	// import "prismjs/components/prism-cpp.js";
-	// import "prismjs/components/prism-bash.js";
-	// import "prismjs/components/prism-python.js";
+	import "prismjs/components/prism-rust.js?inline";
+	import "prismjs/components/prism-pug.js?inline";
+	import "prismjs/components/prism-clike.js?inline";
+	import "prismjs/components/prism-c.js?inline";
+	import "prismjs/components/prism-cpp.js?inline";
+	import "prismjs/components/prism-bash.js?inline";
+	import "prismjs/components/prism-python.js?inline";
 	import "prismjs/themes/prism-dark.min.css";
 
 	/**
