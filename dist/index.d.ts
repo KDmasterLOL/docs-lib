c20414639f1925e8e3ac13f3717c9dab4aff5760
Ivan <ivantolmachevmaster@gmail.com>
2023-11-10 08:37:48 +0300

    change

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,5 @@
-/// <reference types="svelte" />
 export { default as Switcher } from "./components/Switcher.svelte";
 import CodeBlock from './components/CodeBlock.svelte';
 import Collapse from './components/Collapse.svelte';
 import Math from './components/Math.svelte';
-import Settings from './components/Settings.svelte';
-export { CodeBlock, Collapse, Math, Settings };
+export { CodeBlock, Collapse, Math };
