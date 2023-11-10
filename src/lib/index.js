1b86370d4d17611be07b7837bd7a508d21992474
Ivan <ivantolmachevmaster@gmail.com>
2023-11-10 08:36:48 +0300

    change

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,11 +2,9 @@
 import CodeBlock from './components/CodeBlock.svelte'
 import Collapse from './components/Collapse.svelte'
 import Math from './components/Math.svelte'
-import Settings from './components/Settings.svelte'
 export { default as Switcher } from './components/Switcher.svelte'
 export {
 	CodeBlock,
 	Collapse,
 	Math,
-	Settings
 }
